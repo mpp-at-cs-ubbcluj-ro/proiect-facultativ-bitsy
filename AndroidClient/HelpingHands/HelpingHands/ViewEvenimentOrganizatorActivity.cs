3c55e74a279489e787ab6dbe22cd0e51d8f45f04
stefnmUBB <liviu.neacsu@stud.ubbcluj.ro>
2023-06-02 11:31:20 +0300

    date picker shows from the first click

--- AndroidClient/HelpingHands/HelpingHands/ViewEvenimentOrganizatorActivity.cs
+++ AndroidClient/HelpingHands/HelpingHands/ViewEvenimentOrganizatorActivity.cs
@@ -3,6 +3,7 @@ using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
+using Android.Views.InputMethods;
 using Android.Widget;
 using HelpingHands.Adapters;
 using HelpingHands.Data;
@@ -13,19 +14,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Android.App.DatePickerDialog;
 
 namespace HelpingHands
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
-    internal class ViewEvenimentOrganizatorActivity : Activity
+    internal class ViewEvenimentOrganizatorActivity : Activity, IOnDateSetListener
     {
         Eveniment Eveniment;
 
         EditText VolEvenimentName;
-        EditText VolEvenimentDescription;
-        TextView VolEvenimentDate;
+        EditText VolEvenimentDescription;        
         Button VolButtonAddParticipant;
         ListView ParticipantListView;
+        Spinner InterestBox;
+        EditText StartDateBox;
+        EditText EndDateBox;
+        EditText TargetDateBox;
+        EditText LocatieBox;
 
         List<Participant> Participants = new List<Participant>();
 
@@ -41,20 +47,60 @@ namespace HelpingHands
             ParticipantListView = FindViewById<ListView>(Resource.Id.ParticipantListView);
 
             VolEvenimentName = FindViewById<EditText>(Resource.Id.VolEvenimentName);
-            VolEvenimentDescription = FindViewById<EditText>(Resource.Id.VolEvenimentDescription);
-            VolEvenimentDate = FindViewById<TextView>(Resource.Id.VolEvenimentDate);
+            VolEvenimentDescription = FindViewById<EditText>(Resource.Id.VolEvenimentDescription);            
             VolButtonAddParticipant = FindViewById<Button>(Resource.Id.VolButtonAddParticipant);
+            InterestBox = FindViewById<Spinner>(Resource.Id.InterestBox);
+
+            StartDateBox = FindViewById<EditText>(Resource.Id.StartDateBox);
+            EndDateBox = FindViewById<EditText>(Resource.Id.EndDateBox);
+            LocatieBox = FindViewById<EditText>(Resource.Id.LocatieBox);
+            
 
             VolEvenimentName.Text = Eveniment.Name;
             VolEvenimentDescription.Text = Eveniment.Description;
-            VolEvenimentDate.Text = Eveniment.StartDate.ToString("dd.MM.yyyy") + " - " + Eveniment.EndDate.ToString("dd.MM.yyyy");
+            StartDateBox.Text = Eveniment.StartDate.ToString("dd.MM.yyyy");
+            EndDateBox.Text = Eveniment.EndDate.ToString("dd.MM.yyyy");
+            LocatieBox.Text = Eveniment.Location;
 
             VolButtonAddParticipant.Click += VolButtonAddParticipant_Click;
 
             ParticipantListView.SetOnTouchListener(new ListTouchListener());
 
+            StartDateBox.Click += DateBox_Click;
+            EndDateBox.Click += DateBox_Click;
+            StartDateBox.Touch += DateBox_Touch;
+            EndDateBox.Touch += DateBox_Touch;            
+            StartDateBox.FocusChange += DateBox_FocusChange;
+            EndDateBox.FocusChange += DateBox_FocusChange;
+
             Task.Run(Load);
         }
+
+        private void DateBox_FocusChange(object sender, View.FocusChangeEventArgs e)
+        {
+            if (!e.HasFocus) return;
+            TargetDateBox = sender as EditText;
+            var dateTimeNow = DateTime.Now;
+            DatePickerDialog datePicker = new DatePickerDialog(this, this, dateTimeNow.Year, dateTimeNow.Month, dateTimeNow.Day);
+            datePicker.Show();
+        }
+
+        private void DateBox_Touch(object sender, View.TouchEventArgs e)
+        {
+            View view = this.CurrentFocus;
+            if (view != null)
+            {
+                InputMethodManager imm = (InputMethodManager)GetSystemService(Context.InputMethodService);
+                imm.HideSoftInputFromWindow(view.WindowToken, 0);
+            }
+
+            var input = (sender as EditText);
+            var inType = input.InputType;
+            input.InputType = Android.Text.InputTypes.Null;
+            input.OnTouchEvent(e.Event);
+            input.InputType = inType;
+            e.Handled = true;
+        }       
 
         class ListTouchListener : Java.Lang.Object, ListView.IOnTouchListener
         {
@@ -85,6 +131,16 @@ namespace HelpingHands
             {
                 RunOnUiThread(async () =>
                 {
+                    Console.WriteLine("Getting interests");
+                    var interests = (await API.Client.GetInterests()).ToList();
+                    InterestBox.Adapter = new InterestAdapter(this, interests);
+
+                    if (Eveniment.Interests.Length > 0) 
+                    {
+                        var interest = interests.Where(_ => _.Name == Eveniment.Interests[0]).First();
+                        InterestBox.SetSelection(interests.IndexOf(interest));
+                    }
+
                     Console.WriteLine("Getting participants");
                     Participants = (await API.Client.GetParticipants(Eveniment.Id)).ToList();
 
@@ -104,6 +160,19 @@ namespace HelpingHands
         private void VolButtonAddParticipant_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void DateBox_Click(object sender, EventArgs e)
+        {
+            TargetDateBox = sender as EditText;
+            var dateTimeNow = DateTime.Now;
+            DatePickerDialog datePicker = new DatePickerDialog(this, this, dateTimeNow.Year, dateTimeNow.Month, dateTimeNow.Day);
+            datePicker.Show();
+        }
+
+        public void OnDateSet(DatePicker view, int year, int month, int dayOfMonth)
+        {
+            TargetDateBox.Text = new DateTime(year, month, dayOfMonth).ToString("dd.MM.yyyy");
         }
     }
 }
