9153c2f1ff0746376912bd90b4a7e3867a235a85
PaulaBeatrice <serbanpaula002@gmail.com>
2023-06-05 16:58:56 +0300

    xp points

--- HelpingHands/repo/src/main/java/com/helpinghands/repo/VoluntarRepo.java
+++ HelpingHands/repo/src/main/java/com/helpinghands/repo/VoluntarRepo.java
@@ -1,9 +1,22 @@
 package com.helpinghands.repo;
 
+import com.helpinghands.domain.Eveniment;
 import com.helpinghands.domain.Voluntar;
+import com.helpinghands.repo.data.EventOrderOption;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 public class VoluntarRepo extends AbstractUtilizatorRepo<Voluntar> implements IVoluntarRepo {
     public VoluntarRepo() {
         super(Voluntar.class);
+    }
+
+
+    @Override
+    public Integer modifyPoints(Voluntar vol, Integer amount) {
+        int points = vol.getXpPoints() + amount;
+        vol.setXpPoints(points);
+        update(vol);
+        return points;
     }
 }
